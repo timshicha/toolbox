fdaa1acb2801b09cac28407c7439b38599745cd1
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-10-13 23:54:45 -0700

    new style for gate buttons

--- fe/src/components/Buttons.jsx
+++ fe/src/components/Buttons.jsx
@@ -3,7 +3,7 @@ function LogicGateButton({ image, size="50px", onClick }) {
     
     return(
         <>
-            <button onClick={onClick}>
+            <button onClick={onClick} className="bg-gray-300 hover:bg-gray-400 rounded-lg mx-2">
                 <img src={image} width={size} height={size} />
             </button>
         </>
