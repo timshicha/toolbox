a3dd545937e92079a2967dbe3053bcf334c6981f
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-10-19 15:23:33 -0700

    button border makes easier to see which tool is selected

--- fe/src/components/Buttons.jsx
+++ fe/src/components/Buttons.jsx
@@ -12,27 +12,30 @@ class LogicGateButton extends React.Component {
         this.defaultColor = 'bg-gray-300';
         this.selectedColor = 'bg-blue-300';
         this.state = {
-            buttonColor: this.defaultColor
+            buttonColor: this.defaultColor,
+            border: 'border-transparent'
         }
     }
 
     selectTool() {
         // this.buttonColor = this.selectedColor;
         this.setState({
-            buttonColor: this.selectedColor
+            buttonColor: this.selectedColor,
+            border: 'border-blue-400'
         });
     }
 
     deselectTool() {
         this.setState({
-            buttonColor: this.defaultColor
+            buttonColor: this.defaultColor,
+            border: 'border-transparent'
         });
     }
 
     render() {
         return (
             <>
-                <button onClick={this.onClick} className={this.state.buttonColor + " hover:bg-gray-400 rounded-lg " + this.className}>
+                <button onClick={this.onClick} className={this.state.buttonColor + " " + this.state.border + " hover:bg-gray-400 rounded-lg active:bg-gray-500 border-solid border-[3px] " + this.className}>
                     <img src={this.image} width={this.size} height={this.size} />
                 </button>
             </>
